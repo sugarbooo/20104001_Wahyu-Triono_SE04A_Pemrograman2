3d06c08caf4518d7bf8ed6672e46ca22b281d567
Wahyu Triono <70625779+surezluvy@users.noreply.github.com>
2021-11-24 22:08:49 +0700

    Add files via upload

---
+++ modul7/percobaan/Mahasiswa.java
@@ -0,0 +1,11 @@
+package modul7.latihan;
+
+public class Mahasiswa {
+    public void info() {
+        System.out.println("Haloo.. Aku Mahasiswa!");
+    }
+
+    public void info(String nama) {
+        System.out.println("Haloo.. Namaku " + nama);
+    }
+}
---
+++ modul7/percobaan/Penguin.java
@@ -0,0 +1,8 @@
+package modul7.latihan;
+
+public class Penguin extends Burung {
+    @Override
+    public void terbang() {
+        System.out.println("Aku tidak bisa terbang!");
+    }
+}
